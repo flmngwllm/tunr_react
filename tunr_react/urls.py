17a04bd62c38926d3d639e3f0800bd0ead356e8a
Will <flmngwllm232@gmail.com>
2018-12-06 12:08:26 -0500

    django react app set up

--- tunr_react/urls.py
+++ tunr_react/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from . import views
+from django.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    url(r'^$', views.FrontendAppView.as_view())  # New URL for the index route
 ]
